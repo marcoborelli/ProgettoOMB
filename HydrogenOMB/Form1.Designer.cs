a929e52b947f3ba4cf7719e3dae71dc1fbd48732
marcoborelli <marco05.borelli@gmail.com>
2023-02-18 15:18:09 +0100

    Prime prove con gridView

---
+++ HydrogenOMB/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace HydrogenOMB {
+    partial class Form1 {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.startBut = new System.Windows.Forms.Button();
+            this.stopBut = new System.Windows.Forms.Button();
+            this.timerLab = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBut
+            // 
+            this.startBut.Location = new System.Drawing.Point(12, 12);
+            this.startBut.Name = "startBut";
+            this.startBut.Size = new System.Drawing.Size(178, 54);
+            this.startBut.TabIndex = 0;
+            this.startBut.Text = "INIZIA";
+            this.startBut.UseVisualStyleBackColor = true;
+            this.startBut.Click += new System.EventHandler(this.startBut_Click);
+            // 
+            // stopBut
+            // 
+            this.stopBut.Location = new System.Drawing.Point(12, 136);
+            this.stopBut.Name = "stopBut";
+            this.stopBut.Size = new System.Drawing.Size(178, 54);
+            this.stopBut.TabIndex = 1;
+            this.stopBut.Text = "TERMINA";
+            this.stopBut.UseVisualStyleBackColor = true;
+            // 
+            // timerLab
+            // 
+            this.timerLab.AutoSize = true;
+            this.timerLab.Location = new System.Drawing.Point(12, 95);
+            this.timerLab.Name = "timerLab";
+            this.timerLab.Size = new System.Drawing.Size(41, 13);
+            this.timerLab.TabIndex = 2;
+            this.timerLab.Text = "TIMER";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(239, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(350, 441);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 481);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.timerLab);
+            this.Controls.Add(this.stopBut);
+            this.Controls.Add(this.startBut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBut;
+        private System.Windows.Forms.Button stopBut;
+        private System.Windows.Forms.Label timerLab;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ HydrogenOMB/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace HydrogenOMB {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            DataGridViewColumn timer = new DataGridViewTextBoxColumn();
+            timer.HeaderText = "TIMER";
+            dataGridView1.Columns.Add(timer);
+
+            DataGridViewColumn tr1 = new DataGridViewTextBoxColumn();
+            tr1.HeaderText="TRIMMER 1";
+            dataGridView1.Columns.Add(tr1);
+
+            DataGridViewColumn tr2 = new DataGridViewTextBoxColumn();
+            tr2.HeaderText = "TRIMMER 2";
+            dataGridView1.Columns.Add(tr2);
+
+        }
+
+        private void startBut_Click(object sender, EventArgs e) {
+            dataGridView1.Rows.Insert(0, $"{DateTime.Now.Minute}:{DateTime.Now.Second}:{DateTime.Now.Millisecond}", "aaa","bbb");
+        }
+    }
+}
