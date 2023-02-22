683b548d58308020b612f89c398c5bec15902b09
marcoborelli <marco05.borelli@gmail.com>
2023-02-22 16:49:30 +0100

    Risolta eccezione inaspettata se non riceve i dati. Aggiunti controlli disabled su bottoni in fase di lettura dati

--- ProgettoFormLetturaDati/HydrogenOMB/Form1.cs
+++ ProgettoFormLetturaDati/HydrogenOMB/Form1.cs
@@ -18,17 +18,18 @@ namespace HydrogenOMB {
 
         bool first = true;
         DateTime tempo = DateTime.Now, tempoOld = DateTime.Now, oraInizio;
-        TimeSpan deltaTempo, tempoDaStart;
+        TimeSpan deltaTempo;
 
         SerialPort portaSeriale = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
 
         private void Form1_Load(object sender, EventArgs e) {
             timer1.Stop();
+            stopBut.Enabled = false;
             portaSeriale.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived); /*set the event handler*/
             timer1.Enabled = false;
 
             dataGridView1.Columns.Add("delta", "DELTA");
-            dataGridView1.Columns.Add("timer","TIMER");
+            dataGridView1.Columns.Add("timer","MINUTI");
             dataGridView1.Columns.Add("tr1", "TRIMMER 1");
             dataGridView1.Columns.Add("tr2", "TRIMMER 2");
         }
@@ -41,29 +42,36 @@ namespace HydrogenOMB {
         private void stopBut_Click(object sender, EventArgs e) {/*termina*/
             portaSeriale.Close();
             timer1.Stop();
+            stopBut.Enabled = false;
+            startBut.Enabled = true;
         }
 
         private void startBut_Click(object sender, EventArgs e) { /*inizia*/
             timer1.Start();
             tempo = DateTime.Now;
             oraInizio = tempo;
+            stopBut.Enabled = true;
+            startBut.Enabled = false;
             portaSeriale.Open();
         }
 
         private void port_DataReceived(object sender, SerialDataReceivedEventArgs e) {
             //System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
             tempo = DateTime.Now;
-            tempoDaStart = tempo - oraInizio;
             string ricevuto = portaSeriale.ReadLine();
             string[] fields = ricevuto.Split(';');
+
+            if (fields.Length != 2) {
+                fields = new string[] { "-", "-" };
+            }
 
             if (InvokeRequired) { // after we've done all the processing, 
                 this.Invoke(new MethodInvoker(delegate {
                     if (first) {
-                        this.dataGridView1.Rows.Insert(0, "", $"{tempoDaStart.Minutes}:{tempoDaStart.Seconds}:{tempoDaStart.Milliseconds}", fields[0], fields[1]);
+                        this.dataGridView1.Rows.Insert(0, "", $"{tempo.Minute}:{tempo.Second}:{tempo.Millisecond}", fields[0], fields[1]);
                     } else {
                         deltaTempo = tempo - tempoOld;
-                        this.dataGridView1.Rows.Insert(0, $"{deltaTempo.Minutes}:{deltaTempo.Seconds}:{deltaTempo.Milliseconds}", $"{tempoDaStart.Minutes}:{tempoDaStart.Seconds}:{tempoDaStart.Milliseconds}", fields[0], fields[1]);
+                        this.dataGridView1.Rows.Insert(0, $"{deltaTempo.Minutes}:{deltaTempo.Seconds}:{deltaTempo.Milliseconds}", $"{tempo.Minute}:{tempo.Second}:{tempo.Millisecond}", fields[0], fields[1]);
                     }
                     tempoOld = tempo;
                     first = false;
