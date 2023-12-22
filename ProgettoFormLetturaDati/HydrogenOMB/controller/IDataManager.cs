a0e78d060df08980817f0af8f3a9cd767d42eb34
marcoborelli <marco05.borelli@gmail.com>
2023-12-22 21:14:31 +0100

    controller\IDataManager: implementata interfaccia da applicare su classe DataManager per scopi futuri

---
+++ ProgettoFormLetturaDati/HydrogenOMB/controller/IDataManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace HydrogenOMB {
+    public interface IDataManager {
+        void OnStart();
+        void OnEndOpen();
+        void OnStop();
+        void OnForcedStop();
+        void OnEndArrayOpen();
+        void OnEndArrayClose();
+        void OnData(List<string> fields);
+    }
+}
