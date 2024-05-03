a1118154a7745a4c5190864e47d27ba21cec0e1e
marcoborelli <marco05.borelli@gmail.com>
2024-05-03 17:42:32 +0200

    Aggiunta classe che rappresenta i valori che caratterizzano il test di una valvola

---
+++ ProgettoFormLetturaDati/HydrogenOMB/model/CharacteristicValues.cs
@@ -0,0 +1,19 @@
+﻿namespace HydrogenOMB {
+    public class CharacteristicValues {
+        public float Bto { get; private set; } //break to open (0)
+        public float Runo { get; private set; } //run open (45)
+        public float Eto { get; private set; } //end to open (90)
+        public float Btc { get; private set; } //break to close (90)
+        public float Runc { get; private set; } //run close (45)
+        public float Etc { get; private set; } //end to close (0)
+
+        public CharacteristicValues(float bto, float runo, float eto, float btc, float runc, float etc) {
+            Bto = bto;
+            Runo = runo;
+            Eto = eto;
+            Btc = btc;
+            Runc = runc;
+            Etc = etc;
+        }
+    }
+}
