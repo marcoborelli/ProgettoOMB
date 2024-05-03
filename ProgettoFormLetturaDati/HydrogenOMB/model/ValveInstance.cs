0d8bee276eb1f891876809c8cb1ed2ad078d0ce5
marcoborelli <marco05.borelli@gmail.com>
2024-05-03 17:46:00 +0200

    Aggiunta classe che rappresenta un esemplare di valvola

---
+++ ProgettoFormLetturaDati/HydrogenOMB/model/ValveInstance.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace HydrogenOMB {
+    internal class ValveInstance {
+        private string _id, _jobNumber;
+        private ValveModel _valveModel;
+
+
+        [JsonProperty("_id")]
+        public string Id {
+            get => _id;
+            private set => PublicData.InsertIfObjValid(ref _id, value, "Id");
+        }
+
+        [JsonProperty("job_number")]
+        public string JobNumber {
+            get => _jobNumber;
+            private set => PublicData.InsertIfObjValid(ref _jobNumber, value, "Job Number");
+        }
+
+        [JsonProperty("valve_model")]
+        public ValveModel ValveModel {
+            get => _valveModel;
+            private set => PublicData.InsertIfObjValid(ref _valveModel, value, "Valve model");
+        }
+    }
+}
