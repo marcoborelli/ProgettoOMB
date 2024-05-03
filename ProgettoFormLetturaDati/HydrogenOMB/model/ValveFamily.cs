109b56ee902761af6d81de6d2bec61260bb39ece
marcoborelli <marco05.borelli@gmail.com>
2024-05-03 17:43:36 +0200

    Aggiunta classe che rappresenta una famiglia di valvole

---
+++ ProgettoFormLetturaDati/HydrogenOMB/model/ValveFamily.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace HydrogenOMB {
+    public class ValveFamily {
+        private string _id;
+        private CharacteristicValues _theoricValues;
+
+
+        [JsonProperty("_id")]
+        public string Id {
+            get => _id;
+            private set => PublicData.InsertIfObjValid(ref _id, value, "Id");
+        }
+
+        [JsonProperty("theoric_values")]
+        public CharacteristicValues TheoricValues {
+            get => _theoricValues;
+            private set => PublicData.InsertIfObjValid(ref _theoricValues, value, "Theoric values");
+        }
+    }
+}
