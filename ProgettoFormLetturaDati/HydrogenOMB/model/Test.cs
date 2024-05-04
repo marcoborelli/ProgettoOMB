8063db172fe22929dfa6886df4c857c9bc70c409
marcoborelli <marco05.borelli@gmail.com>
2024-05-04 16:47:28 +0200

    Aggiunta classe che rappresenta un test su una valvola

---
+++ ProgettoFormLetturaDati/HydrogenOMB/model/Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace HydrogenOMB {
+    public class Test {
+        private object _instanceId;
+        private List<OMBRecord> _data;
+
+
+        [JsonProperty("instance_id")]
+        public object InstanceId {
+            get => _instanceId;
+            private set {
+                if (value is JObject) {
+                    _instanceId = ((JObject)value).ToObject<ValveInstance>();
+                } else {
+                    _instanceId = value.ToString();
+                }
+            }
+        }
+
+
+        [JsonProperty("data")]
+        public List<OMBRecord> Data {
+            get => _data;
+            set => PublicData.InsertIfObjValid(ref _data, value, "Data (OmbRecord)");
+        }
+
+
+        public Test(object instanceId, List<OMBRecord> data) {
+            InstanceId = instanceId;
+            Data = data;
+        }
+    }
+}
